3957d96b305f4af355cb67b9b0cfceeba6bf508b
Shahidul Hasan <er.shahidul@gmail.com>
2021-06-15 00:38:56 +0600

    test for hidden file

---
+++ .index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="task1/css/bootstrap.min.css">
+    <style type="text/css">
+        .container{
+            margin-top: 100px;
+        }
+
+    </style>
+</head>
+<body>
+
+<div class="container">
+    <div class="list-group">
+        <a href="task1/form.php" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Task-1) Create a php form with 3 steps or pages. It should have a progress bar at the top of the page.</h5>
+            </div>
+            <p class="mb-1 bold">Note: Click</p>
+        </a>
+        <a href="task2/table.php" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Task-2) Create a page that has an html table (you can use any kind of dummy data) and an image with an "Export to PDF" button that will create a pdf version of the page.</h5>
+            </div>
+            <p class="mb-1">Note: Click</p>
+        </a>
+        <a href="#" class="list-group-item list-group-item-action flex-column align-items-start">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Task-3) Create a command line script that will fetch and validate the local and external ip addresses used by the sytem. The ip addresses retrieved should be validated and output on the comand line.</h5>
+            </div>
+            <p class="mb-1">Note: Open Command Line -> cd task/task3/ -> php retrieveValidIP.php</p>
+        </a>
+    </div>
+</div>
+
+</body>
+</html>
